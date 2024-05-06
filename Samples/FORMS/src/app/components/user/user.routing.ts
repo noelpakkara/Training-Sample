d96bad090e1e64c1b3ae6dfe854b8046737567b1
noelpakkara <88715247+noelpakkara@users.noreply.github.com>
2024-05-06 15:31:28 +0530

    location changed

--- Samples/FORMS/src/app/components/user/user.routing.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserListComponent } from './user-list/user-list.component';
-
-export const userRoutes: Routes = [{ path: '', component: UserListComponent }];
